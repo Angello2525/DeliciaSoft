a3071715f0c5753816e323c454ef5f7f9d768e88
Danna <dannafrreira1054@gmail.com>
2025-04-07 16:32:03 -0500

    migracion de base de datos y vista de cartas

---
+++ DeliciaSoft/Models/Rol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeliciaSoft.Models;
+
+public partial class Rol
+{
+    public int IdRol { get; set; }
+
+    public string? Rol1 { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public bool? Estado { get; set; }
+
+    public virtual ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+}
